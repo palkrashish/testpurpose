cc39cddb026bdb64e96a98dc771a1e4e252f0536
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-04 00:19:57 +0530

    feat:- style the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Api from "./components/Api";
-import Axios from "./components/Axios"
+import Axios from "./components/Axios";
 const App = () => {
   return (
     <div className="container">
-      <h1>Test :- </h1>
+      <h1 className="font-bold text-[30px] p-2 m-2 border border-slate-800 bg-gray-500 w-15 h-16 rounded-lg text-center ">
+        Welcome to Machine round Questions
+      </h1>
+      {/* Example of API fetching  */}
       {/* <Api /> */}
       <Axios />
     </div>
