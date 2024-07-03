f226c1afb73ee35a744ae04ae38c0ad905436c4b
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-03 23:58:34 +0530

    feat:- installed axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Api from "./components/Api";
+import Axios from "./components/Axios"
 const App = () => {
   return (
     <div className="container">
       <h1>Test :- </h1>
       <Api />
+      <Axios />
     </div>
   );
 };
---
+++ src/components/Axios.jsx
@@ -0,0 +1,9 @@
+const Axios = () => {
+  return (
+    <div>
+      <h1> example of Axios</h1>
+    </div>
+  );
+};
+
+export default Axios;
