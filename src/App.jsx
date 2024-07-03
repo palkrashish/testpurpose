5d856e7621f89f488a06f3f42997407b9a3a0746
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-04 00:10:58 +0530

    feat:- using axios  fetching data api

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="container">
       <h1>Test :- </h1>
-      <Api />
+      {/* <Api /> */}
       <Axios />
     </div>
   );
--- src/components/Axios.jsx
+++ src/components/Axios.jsx
@@ -1,13 +1,28 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 const Axios = () => {
-    useEffect(()=>{
-        axios.get("")
-        .then((res)=>{})
-    })
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      console.log(res);
+      setData(res.data);
+    });
+  }, [setData]);
   return (
     <div>
-      <h1> example of Axios</h1>
+      <h1 className="font-bold"> Example of Axios</h1>
+      {data.map((msg) => {
+        const { id, title, body } = msg;
+        return (
+          <div>
+            <ol>
+              <li> {id} </li>
+              <li> {body} </li>
+              <li> {title} </li>
+            </ol>{" "}
+          </div>
+        );
+      })}
     </div>
   );
 };
