1030c12f295dec236156f5f1688c1b8c29262a35
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2024-07-03 23:59:48 +0530

    feat:- use

--- src/components/Axios.jsx
+++ src/components/Axios.jsx
@@ -1,4 +1,10 @@
+import axios from "axios";
+import { useEffect } from "react";
 const Axios = () => {
+    useEffect(()=>{
+        axios.get("")
+        .then((res)=>{})
+    })
   return (
     <div>
       <h1> example of Axios</h1>
